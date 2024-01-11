a8331980223bba94d16d10576a188aa665a2287d
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2024-01-11 23:02:46 +0100

    Dodelani

--- StachRPR/Form1.cs
+++ StachRPR/Form1.cs
@@ -13,38 +13,99 @@ namespace StachRPR
 {
     public partial class Form1 : Form
     {
+        int[,] pole;
+        int sloupec;
+        int radek;
+        int pocetRobotu;
+        int pocetPrijmu;
+        int pocetKoncu;
         public Form1()
         {
             InitializeComponent();
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Dock = DockStyle.Fill;
-            GenerovaniMrizky(70, 40, 20, pictureBox1);
-            Panel bomba = new Panel();
-            bomba.Size = new Size();
-            Controls.Add(bomba);
-            Controls.Add(pictureBox1);
+            pictureBox1.Location = new Point(0, 40);
+            //pictureBox1.Dock = DockStyle.Fill;
+            sloupec = 70;
+            radek = 40;
+
+            pocetRobotu = 2;
+            pocetPrijmu = 5;
+            pocetKoncu= 10;
+            
+
+            pole = new int[sloupec,radek];
+            VyplnNulou(pole, sloupec, radek);
+
+            
+
+            GenerovaniMrizky(sloupec, radek, 20, pictureBox1, pole);
+
+
+
         }
 
         Panel okno = new Panel();
 
         /// ////////////////////////////////////////////////
 
-        public void GenerovaniMrizky(int sloupec, int radek, int velikost, PictureBox picturebox)
+        public void GenerovaniMrizky(int sloupec, int radek, int velikost, PictureBox picturebox, int[,] pole)
         {
             Bitmap bitmap = new Bitmap(sloupec * velikost, radek * velikost);
 
-            for (int x = 0; x <= sloupec; x++)
+            for (int x = 0; x < sloupec; x++)
             {
                 Graphics g = Graphics.FromImage(bitmap);
                 Pen pen = new Pen(Color.Black);
-                for (int y = 0; y <= radek; y++)
+                for (int y = 0; y < radek; y++)
                 {
+                    if (pole[x, y] == 1)
+                    {
+                        // ROBOT
+                        int circleX = x * velikost;
+                        int circleY = y * velikost;
+                        int circleSize = velikost;
+
+                        
+                        g.FillEllipse(Brushes.Red, circleX, circleY, circleSize, circleSize);
+                    }
+
+                    if (pole[x, y] == 2)
+                    {
+                        // PRIJEM
+                        int squareX = x * velikost;
+                        int squareY = y * velikost;
+                        int squareSize = velikost;
 
 
-                    g.DrawLine(pen, 0, y * velikost, sloupec * velikost, y * velikost);
+                        g.FillRectangle(Brushes.Yellow, squareX, squareY, squareSize, squareSize);
+
+                        
+                    }
+
+                    if (pole[x, y] == 3)
+                    {
+                        //MISTO ODEVZDANI
+                        int squareX = x * velikost;
+                        int squareY = y * velikost;
+                        int squareSize = velikost;
+
+
+                        g.FillRectangle(Brushes.Green, squareX, squareY, squareSize, squareSize);
+
+                        string text = "1";
+                        Font font = new Font("Arial", 10, FontStyle.Regular);
+                        Brush textBrush = Brushes.Black; // You can change the color of the text
+
+                        // Draw the text in the center of the square
+                        float textX = squareX + (squareSize - g.MeasureString(text, font).Width) / 2;
+                        float textY = squareY + (squareSize - g.MeasureString(text, font).Height) / 2;
+
+                        g.DrawString(text, font, textBrush, textX, textY);
+                    }
+
+                    g.DrawLine(pen, 0 , y * velikost, sloupec * velikost, y * velikost);
                 }
 
-                g.DrawLine(pen, x * velikost, 0, x * velikost, radek * velikost);
+                g.DrawLine(pen, x * velikost , 0, x * velikost, radek * velikost);
             }
 
             picturebox.Image = bitmap;
@@ -53,14 +114,112 @@ namespace StachRPR
 
         }
 
+        public void VyplnNulou(int[,] pole, int sloupec,int radek) 
+        {
+            for (int x = 0; x < sloupec; x++)
+            {
+                
+                for (int y = 0; y < radek; y++)
+                {
+
+
+                    pole[x, y] = 0;
+                }
+
+                
+            }
+        }
+
+        public void GenerovaniRobotu(int[,] poles, int sloupec, int radek, int pocetrobotu) 
+        {
+            
+            Random x = new Random();
+            Random y = new Random();
+            int xrnd = x.Next(sloupec);
+            int yrnd = y.Next(radek);
+            for (int i = 0;i < pocetrobotu;)
+            {
+                xrnd = x.Next(sloupec);
+                yrnd = y.Next(radek);
+                if (poles[xrnd,yrnd] == 0) 
+                {
+                    poles[xrnd, yrnd] = 1;
+                    Console.WriteLine("Robot: " + i + " Pozice: " + xrnd + " " + yrnd);
+                    i++;
+                }
+                
+            }
+        }
+
+        public void GenerovaniPrijmu(int[,] poles, int sloupec, int radek, int pocetprijmu)
+        {
+
+            Random x = new Random();
+            Random y = new Random();
+            int xrnd = x.Next(sloupec);
+            int yrnd = y.Next(radek);
+            for (int i = 0; i < pocetprijmu;)
+            {
+                xrnd = x.Next(sloupec);
+                yrnd = y.Next(radek);
+                if (poles[xrnd, yrnd] == 0)
+                {
+                    poles[xrnd, yrnd] = 2;
+                    Console.WriteLine("Prijem: " + i + " Pozice: " + xrnd + " " + yrnd);
+                    i++;
+                }
+
+            }
+        }
+
+        public void GenerovaniKoncu(int[,] poles, int sloupec, int radek, int pocetkoncu)
+        {
+
+            Random x = new Random();
+            Random y = new Random();
+            int xrnd = x.Next(sloupec);
+            int yrnd = y.Next(radek);
+            for (int i = 0; i < pocetkoncu;)
+            {
+                xrnd = x.Next(sloupec);
+                yrnd = y.Next(radek);
+                if (poles[xrnd, yrnd] == 0)
+                {
+                    poles[xrnd, yrnd] = 3;
+                    Console.WriteLine("Konec: " + i + " Pozice: " + xrnd + " " + yrnd);
+                    i++;
+                }
+
+            }
+        }
+
+
+
+
+
         private void button1_Click(object sender, EventArgs e)
         {
-            //okno.Location = new Point(0, 30);
-            //okno.Visible = true;
-            //okno.Visible = false;
-            
-            
+            GenerovaniRobotu(pole, sloupec, radek, pocetRobotu);
+            GenerovaniPrijmu(pole, sloupec, radek, pocetPrijmu);
+            GenerovaniKoncu(pole, sloupec, radek, pocetKoncu);
+            for (int y = 0; y< radek; y++)
+            {
 
+                for (int x = 0; x < sloupec; x++)
+                {
+
+
+                    Console.Write(pole[x, y]);
+                }
+                Console.WriteLine();
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GenerovaniMrizky(sloupec, radek, 20, pictureBox1, pole);
         }
     }
 }
